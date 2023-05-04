10d50b2ee5a94b209e646a1c8389f73e07acab2f
ruichen.li <ruichen.li@opay-inc.com>
2023-05-04 18:34:48 +0800

    sad

---
+++ src/fc.ts
@@ -0,0 +1,10 @@
+import {defineComponent, h} from 'vue'
+
+export default defineComponent({
+  name: 'asdas',
+  render() {
+    return h('div', {
+      class: ['text-blue-600']
+    }, 78787878)
+  }
+})
