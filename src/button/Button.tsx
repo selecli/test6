1ae47a7ba4c045bd312cf7a301b1c9b1c4ec1601
ruichen.li <ruichen.li@opay-inc.com>
2023-05-05 11:30:10 +0800

    修改

---
+++ src/button/Button.tsx
@@ -0,0 +1,17 @@
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'button',
+  setup(props, {slots}) {
+    return () => <button class={`py-2 
+      px-4 
+      font-semibold 
+      rounded-lg 
+      shadow-md 
+      text-white 
+      bg-green-500 
+      hover:bg-green-700 
+      border-none 
+      cursor-pointer` }>{slots.default ? slots.default() : '222'}</button>
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -10,6 +11,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
     }
   }
 })
